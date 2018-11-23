da7d69d6624568f5acb8b59bbbc0fb66fbc8d4e1
Dollis <542763565@qq.com>
2018-11-23 19:03:59 +0800

    第二次：模板、session、cookie、response、request

---
+++ src/main/java/com/wust/wenda/controller/IndexController.java
@@ -0,0 +1,75 @@
+package com.wust.wenda.controller;
+
+import com.wust.wenda.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.*;
+
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(path = {"/", "/index"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public String index() {
+        return "Hello Nowcoder";
+    }
+
+    @RequestMapping(path = {"/profile/{groupId}/{userId}"})
+    @ResponseBody
+    public String profile(@PathVariable("userId") int userId,
+                          @PathVariable("groupId") String groupId,
+                          @RequestParam(value = "type", defaultValue = "1") int type,
+                          @RequestParam(value = "key", required = false) String key) {
+        return String.format("Profile Page of %s / %d t:%d k:%s", groupId, userId, type, key);
+    }
+
+    @RequestMapping(path = {"/vm"}, method = {RequestMethod.GET})
+    public String template(Model model) {
+        model.addAttribute("value1", "vvvvv1");
+        List<String> colors = Arrays.asList(new String[]{"RED", "GREEN", "BLUE"});
+        model.addAttribute("colors", colors);
+
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < 4; ++i) {
+            map.put(String.valueOf(i), String.valueOf(i * i));
+        }
+        model.addAttribute("map", map);
+        model.addAttribute("user", new User("LEE"));
+        return "home";
+    }
+
+    @RequestMapping("/request")
+    @ResponseBody
+    public String request(Model model,
+                          HttpServletResponse response,
+                          HttpServletRequest request,
+                          HttpSession httSsession,
+                          @CookieValue("JSESSIONID") String sessionId) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("COOKIEVALUE:" + sessionId + "<br>");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            sb.append(name + ":" + request.getHeader(name) + "<br>");
+        }
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                sb.append("Cookie:" + cookie.getName() + " Value:" + cookie.getValue());
+            }
+        }
+        sb.append(request.getMethod() + "<br>");
+        sb.append(request.getQueryString() + "<br>");
+        sb.append(request.getPathInfo() + "<br>");
+        sb.append(request.getRequestURL() + "<br>");
+
+        response.addHeader("nowcoderId","HelloWust");
+        return sb.toString();
+    }
+}
