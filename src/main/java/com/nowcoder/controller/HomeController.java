c269c7fc226f36ef3393bc0cbe5ba58590e9b608
Dollis <542763565@qq.com>
2018-11-26 08:51:36 +0800

    class_3

---
+++ src/main/java/com/nowcoder/controller/HomeController.java
@@ -0,0 +1,43 @@
+package com.nowcoder.controller;
+
+import com.nowcoder.model.Question;
+import com.nowcoder.model.ViewObject;
+import com.nowcoder.service.QuestionService;
+import com.nowcoder.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    QuestionService questionService;
+
+    @RequestMapping(path = {"/", "/index"}, method = {RequestMethod.GET})
+    public String index(Model model) {
+//        List<Question> questionList = questionService.getLastestQuestions(0, 0, 10);
+//        List<ViewObject> vos = new ArrayList<ViewObject>();
+//
+//        for(Question question : questionList){
+//            ViewObject vo = new ViewObject();
+//            vo.set("question",question);
+//            vo.set("user",userService.getUser(question.getUserId()));
+//        }
+//
+//        model.addAttribute("vos",vos);
+        return "index";
+    }
+
+}
