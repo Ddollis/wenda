1e304d755a76aefb252f2015cb4851395e8ad4f0
Dollis <542763565@qq.com>
2018-11-28 14:04:04 +0800

    changed

--- src/main/java/com/nowcoder/controller/QuestionController.java
+++ src/main/java/com/nowcoder/controller/QuestionController.java
@@ -9,10 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -54,5 +52,14 @@ public class QuestionController {
             logger.error("增加评论失败" + e.getMessage());
         }
         return WendaUtil.getJSONString(1, "失败");
+    }
+
+    @RequestMapping(value = "/question/{qid}", method = RequestMethod.GET)
+    public String questionDetail(Model model,
+                                 @PathVariable("qid") int qid) {
+        Question question = questionService.getById(qid);
+        model.addAttribute("question", question);
+
+        return "detail";
     }
 }
--- src/main/java/com/nowcoder/service/QuestionService.java
+++ src/main/java/com/nowcoder/service/QuestionService.java
@@ -14,6 +14,8 @@ public class QuestionService {
     @Autowired
     QuestionDAO questionDAO;
 
+    @Autowired
+    SensitiveService sensitiveService;
     public Question getById(int id) {
         return questionDAO.getById(id);
     }
@@ -22,7 +24,8 @@ public class QuestionService {
         question.setTitle(HtmlUtils.htmlEscape(question.getTitle()));
         question.setContent(HtmlUtils.htmlEscape(question.getContent()));
         //敏感词过滤
-
+        question.setTitle(sensitiveService.filter(question.getTitle()));
+        question.setContent(sensitiveService.filter(question.getContent()));
         return questionDAO.addQuestion(question) > 0 ? question.getId() : 0;
     }
 
