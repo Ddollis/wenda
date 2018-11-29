4e25ed7352f0fdb38ce0372a8187769c9d3e525e
Dollis <542763565@qq.com>
2018-11-29 14:20:58 +0800

    站内信、发送消息

--- src/main/java/com/nowcoder/controller/CommentController.java
+++ src/main/java/com/nowcoder/controller/CommentController.java
@@ -1,20 +1,23 @@
 package com.nowcoder.controller;
 
-import com.nowcoder.model.Comment;
-import com.nowcoder.model.EntityType;
-import com.nowcoder.model.HostHolder;
+import com.nowcoder.model.*;
 import com.nowcoder.service.CommentService;
+import com.nowcoder.service.MessageService;
 import com.nowcoder.service.QuestionService;
+import com.nowcoder.service.UserService;
 import com.nowcoder.util.WendaUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class CommentController {
@@ -29,15 +32,21 @@ public class CommentController {
     @Autowired
     QuestionService questionService;
 
+    @Autowired
+    MessageService messageService;
+
+    @Autowired
+    UserService userService;
+
 
     @RequestMapping(path = {"/addComment"}, method = {RequestMethod.POST})
     public String addComment(@RequestParam("questionId") int questionId,
                              @RequestParam("content") String content) {
         try {
             Comment comment = new Comment();
-            if(hostHolder.getUser()!=null){
+            if (hostHolder.getUser() != null) {
                 comment.setUserId(hostHolder.getUser().getId());
-            }else{
+            } else {
                 comment.setUserId(WendaUtil.ANONYMOUS_USERID);
             }
             comment.setContent(content);
@@ -48,12 +57,42 @@ public class CommentController {
 
             commentService.addComment(comment);
 
-            int count = commentService.getCommentCount(comment.getEntityId(),comment.getEntityType());
-            questionService.updateCommentCount(comment.getEntityId(),count);
+            int count = commentService.getCommentCount(comment.getEntityId(), comment.getEntityType());
+            questionService.updateCommentCount(comment.getEntityId(), count);
 
         } catch (Exception e) {
             logger.error("增加评论失败" + e.getMessage());
         }
         return "redirect:/question/" + String.valueOf(questionId);
+    }
+
+    @RequestMapping(path = {"/msg/list"}, method = RequestMethod.GET)
+    public String conversationList(Model model) {
+
+        return "letter";
+    }
+
+    @RequestMapping(path = {"/msg/detail"}, method = RequestMethod.GET)
+    public String conversationDetail(Model model,
+                                     @RequestParam("conversationId") String conversationId) {
+        try {
+            List<Message> conversationList = messageService.getConversationDetail(conversationId, 0, 10);
+            List<ViewObject> messages = new ArrayList<>();
+            for (Message message : conversationList) {
+                ViewObject vo = new ViewObject();
+                vo.set("message", message);
+                User user = userService.getUser(message.getFromId());
+                if (user == null) {
+                    continue;
+                }
+                vo.set("headUrl", user.getHeadUrl());
+                vo.set("userId", user.getId());
+                messages.add(vo);
+            }
+            model.addAttribute("messages",messages);
+        } catch (Exception e) {
+            logger.error("获取详情消息页失败" + e.getMessage());
+        }
+        return "letterDetail";
     }
 }
